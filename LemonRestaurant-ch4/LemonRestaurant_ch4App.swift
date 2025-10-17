a1db755d466699a10a49821aea281aabc6f8fca7
Ramone Hayes <ramone.hayes@yahoo.com>
2025-10-16 19:41:34 -0700

    Initial Commit

---
+++ LemonRestaurant-ch4/LemonRestaurant_ch4App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurant_ch4App.swift
+//  LemonRestaurant-ch4
+//
+//  Created by RR HAYES on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurant_ch4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Practices/Variables.playground/Contents.swift
@@ -0,0 +1,44 @@
+
+print("")
+import UIKit
+
+// variables var
+// constants let
+
+// constants
+let day = "Monday"
+let dailyTemp = 75
+print("Today is \(day). And the temperature is \(dailyTemp).")
+
+//variables
+var temp = 70
+print("The temp in the morning is \(temp)")
+temp = 80
+print("The temp at noon is \(temp)")
+
+let weeklyTemp = 75.6
+
+temp = Int(weeklyTemp)
+print("The temp in the evening is \(temp)")
+var isRaining = false
+
+print("----- The game score example -----")
+let levelScore = 10
+var gameScore = 0
+print(levelScore)
+print(gameScore)
+print("The level score is \(levelScore)") // String interpolation
+
+gameScore = 5
+print(gameScore)
+
+gameScore += levelScore
+print(gameScore)
+
+var levelBonusScore = 20.0 // Double
+print("Your bonus score is \(levelBonusScore)")
+let levellowestscore=50
+let levelhighesscore=99
+let levels = 10
+
+print("the lowest score is \(levellowestscore)")
