62ed3f20c20bc16e1543ac2783b0b165d21569ce
RHayes1302 <ramone.hayes@yahoo.com>
2025-10-27 19:30:52 -0700

    assigment3

---
+++ LemonRestaurant-ch4/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  LemonRestaurant-ch4
+//
+//  Created by Ramone Hayes on 10/23/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
